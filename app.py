34d83a62d6a30f39809e06c296f4bd3134a6bc89
Luigi Longo <luigi.longo@imtlucca.it>
2025-03-12 13:07:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("""
     <style>
         .stApp {
             background-color: black;
-            color: red;
+            color: white;
         }
         h1, h2, h3, h4, h5, h6, p, label, div, span {
             color: white !important;
